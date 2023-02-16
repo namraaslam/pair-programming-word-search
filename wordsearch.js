98aef9dde59a48d41488fe138aa6da643c93ddfc
namraaslam <namra.aslam@ucalgary.ca>
2023-02-15 18:27:38 -0700

    pair programming completed

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,18 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((lettersRow) => lettersRow.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  let transposeGrid = letters[0].map((col, i) => letters.map((row) => row[i]));
+
+  const verticalJoin = transposeGrid.map((lettersCol) => lettersCol.join(""));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+
+  return false;
+};
 
 module.exports = wordSearch
